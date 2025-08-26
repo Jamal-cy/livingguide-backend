e11ca3ded7f0ce7d9758fb16a098d67596cdfa40
Jamal-cy <mariaxsandra2@gmail.com>
2025-08-26 00:15:34 -0800

    .

--- src/seedLivingGuides.js
+++ src/seedLivingGuides.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"; 
+dotenv.config(); 
 import { LivingGuide } from "./models/livingGuide.model.js";
 
 const MONGO_URI = process.env.MONGO_URI;
+console.log("MONGO_URI:", MONGO_URI); // DEBUG: should print your Mongo URI
+
+if (!MONGO_URI) {
+  console.error("Error: MONGO_URI is not defined in your .env file");
+  process.exit(1);
+}
 
 const livingGuides = [
   {
